69ac5d149565a7c91eafc9fe3464923d1c3afd8b
sufyan79 <sufyan01000@gmail.com>
2023-01-26 16:39:38 -0500

    best work

--- store/models.py
+++ store/models.py
@@ -5,23 +5,21 @@ from django.db import models
 
 
 class Niche(models.Model):
-    niche_title = models.CharField(max_length=255)
+    niche_title = models.CharField(max_length=255, unique=True)
 
 
 class Category(models.Model):
-    category_title = models.CharField(max_length=255)
-
-    niche_id = models.ForeignKey(Niche, on_delete=models.PROTECT)
+    category_title = models.CharField(max_length=255, unique=True)
+    niche = models.ForeignKey(Niche, on_delete=models.PROTECT)
 
 
 class SubCategory(models.Model):
-    subcategory_title = models.CharField(max_length=255)
-
-    category_id = models.ForeignKey(Category, on_delete=models.PROTECT)
+    subcategory_title = models.CharField(max_length=255, unique=True)
+    category = models.ForeignKey(Category, on_delete=models.PROTECT)
 
 
 class ProductImage(models.Model):
-    image_url = models.FilePathField()
+    image_url = models.URLField(max_length=255)
 
 
 class ProductColor(models.Model):
@@ -30,16 +28,16 @@ class ProductColor(models.Model):
 
 class Product(models.Model):
     product_name = models.CharField(max_length=255)
-    product_price = models.CharField(max_length=255)
-    product_description = models.CharField(max_length=255)
+    product_price = models.DecimalField(max_digits=6, decimal_places=2)
+    product_description = models.TextField(max_length=255)
     product_blog = models.TextField()
 
-    product_img = models.ForeignKey(ProductImage, on_delete=models.CASCADE)
-    color_id = models.ForeignKey(ProductColor, on_delete=models.CASCADE)
-    category_id = models.ForeignKey(SubCategory, on_delete=models.PROTECT)
+    image = models.ForeignKey(ProductImage, on_delete=models.CASCADE)
+    color = models.ForeignKey(ProductColor, on_delete=models.CASCADE)
+    category = models.ForeignKey(SubCategory, on_delete=models.PROTECT)
 
-    product_capacity = models.CharField(max_length=255)
-    product_compatibility = models.CharField(max_length=255)
+    product_capacity = models.CharField(max_length=255, null=True)
+    product_compatibility = models.CharField(max_length=255, null=True)
     product_type = models.CharField(max_length=255, null=True)
-    Product_speed = models.IntegerField()
+    Product_speed = models.IntegerField(null=True)
     product_created_at = models.DateTimeField(auto_now_add=True)
