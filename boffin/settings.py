cde7d29441009b56ef810c766a73f6d49e122942
sufyan79 <sufyan01000@gmail.com>
2023-02-07 14:45:22 -0500

    7 FEB

--- boffin/settings.py
+++ boffin/settings.py
@@ -16,6 +16,13 @@ import os
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
+DEBUG = True
+
+# SECURITY WARNING: keep the secret key used in production secret!
+SECRET_KEY = 'django-insecure-bq(-2(z&z_798r^lm!po=i&cw(rqr1y+ujduk5255fa!_io!&9'
+
+ALLOWED_HOSTS = []
+
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
@@ -127,6 +134,19 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 # custom setting
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+
+# Database
+# https://docs.djangoproject.com/en/4.1/ref/settings/#databases
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'boffin',
+        'HOST': 'localhost',
+        'USER': 'root',
+        'PASSWORD': '123456'
+    }
+}
 
 
 INTERNAL_IPS = [
--- boffin/settings/prod.py
+++
@@ -1,8 +0,0 @@
-from .common import *
-import os
-
-
-DEBUG = False
-SECRET_KEY = os.environ("SECRET_KEY")
-
-ALLOWED_HOSTS = []
